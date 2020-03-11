eab46433e35f8df69854f49fce7c60a891135cb4
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2020-03-11 10:49:55 +0100

    (feature) - add structural directive exercice solution

---
+++ TP/steps/structural-directive-solution/src/app/people/add-dialog/add-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'sfeir-add-dialog',
+  templateUrl: './add-dialog.component.html',
+  styleUrls: ['./add-dialog.component.css']
+})
+export class AddDialogComponent implements OnInit {
+  constructor(public dialogRef: MatDialogRef<AddDialogComponent>) {}
+
+  closeDialog(result = null) {
+    this.dialogRef.close(result);
+  }
+
+  ngOnInit() {}
+
+  onCancel() {
+    this.closeDialog();
+  }
+
+  onSave(person) {
+    this.closeDialog(person);
+  }
+}
---
+++ TP/steps/structural-directive-solution/src/app/shared/display/display.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { NgIfContext } from '@angular/common';
+
+// tslint:disable-next-line: directive-selector
+@Directive({ selector: '[ngDisplay]' })
+export class NgDisplayDirective<T = unknown> {
+  constructor(
+    private readonly templateRef: TemplateRef<NgIfContext<T>>,
+    private readonly viewContainer: ViewContainerRef
+  ) {}
+
+  @Input() set ngDisplay(condition: T) {
+    if (condition) {
+      if (!!condition) {
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainer.clear();
+      }
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+}
