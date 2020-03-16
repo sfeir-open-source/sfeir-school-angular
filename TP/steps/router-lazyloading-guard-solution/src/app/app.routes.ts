36a45e2f21e936d357851e262a2615ba76346762
Jean-Rodolphe POINSOT <jr.poinsot@gmail.com>
2020-03-16 11:03:21 +0100

    (feat) add resolver basic ex in router-lazyloading-guard-solution step

--- TP/steps/router-lazyloading-guard-solution/src/app/app.routes.ts
+++ TP/steps/router-lazyloading-guard-solution/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 // APP COMPONENTS
 import { HomeComponent } from './home/index';
 import { AccessSecretGuard } from './access-secret-guard/access-secret.guard';
+import { UserResolverService } from './resolver/user-resolver.service';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, resolve: { users: UserResolverService } },
   {
     path: 'secret',
     loadChildren: () => import('./secret/secret.module').then(mod => mod.SecretModule),
---
+++ TP/steps/router-lazyloading-guard-solution/src/app/resolver/user-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const BASE_URL = 'http://localhost:9000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserResolverService implements Resolve<any> {
+
+  constructor(private _http: HttpClient) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    return this._http.get(`${BASE_URL}/api/peoples/`);
+  }
+}
