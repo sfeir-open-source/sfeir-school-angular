390a1fbd7ac553029c26594ab7340f9ba7e6e4dd
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2020-11-12 19:27:07 +0100

    fix: step 08-ngfor does not build + add default relative link strategy

--- TP/steps/08-ngfor/src/app/app.routes.ts
+++ TP/steps/08-ngfor/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 
 // APP COMPONENTS
-import { HomeComponent } from './home/index';
+import { HomeComponent } from './home';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: true, relativeLinkResolution: 'legacy' });
+export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: true });
