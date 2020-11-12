5b03f6d6f624e8610cdd24cdb855ea17a6ff371f
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2020-11-12 23:22:33 +0100

    fix: step 26-resolvers-solution does not build + add default relative link strategy

--- TP/steps/26-resolvers-solution/src/app/app.routes.ts
+++ TP/steps/26-resolvers-solution/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 
 // APP COMPONENTS
-import { HomeComponent } from './home/index';
-import { PeopleComponent } from './people/index';
-import { UpdateComponent } from './update/index';
+import { HomeComponent } from './home';
+import { PeopleComponent } from './people';
+import { UpdateComponent } from './update';
 import { UserDetailsResolver } from './shared/resolver/user-details.resolver';
 
 const ROUTES: Routes = [
@@ -13,4 +13,4 @@ const ROUTES: Routes = [
   { path: 'edit/:id', component: UpdateComponent, resolve: { user: UserDetailsResolver } }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: true, relativeLinkResolution: 'legacy' });
+export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: true });
