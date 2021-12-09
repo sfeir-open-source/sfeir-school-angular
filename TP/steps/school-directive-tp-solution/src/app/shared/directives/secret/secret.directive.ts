157cf19e2297372802d3ae405ceae8aca1eb5a9e
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2021-12-09 19:12:44 +0100

    feat(school-directive-tp): :fire: remove deprecated api createComponentFactory

--- TP/steps/school-directive-tp-solution/src/app/shared/directives/secret/secret.directive.ts
+++ TP/steps/school-directive-tp-solution/src/app/shared/directives/secret/secret.directive.ts
@@ -1,31 +1,14 @@
-import {
-  ComponentFactory,
-  ComponentFactoryResolver,
-  ComponentRef,
-  Directive,
-  OnInit,
-  TemplateRef,
-  ViewContainerRef
-} from '@angular/core';
+import { ComponentRef, Directive, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 import { IconSecretComponent } from './icon-secret/icon-secret.component';
 
 @Directive({
   selector: '[sfeirSecret]'
 })
 export class SecretDirective implements OnInit {
-  constructor(
-    private readonly componentFactoryResolver: ComponentFactoryResolver,
-    private readonly templateRef: TemplateRef<any>,
-    private readonly viewContainerRef: ViewContainerRef
-  ) {}
+  constructor(private readonly templateRef: TemplateRef<any>, private readonly viewContainerRef: ViewContainerRef) {}
 
   ngOnInit(): void {
-    const iconComponentFactory: ComponentFactory<IconSecretComponent> = this.componentFactoryResolver.resolveComponentFactory(
-      IconSecretComponent
-    );
-    const iconComponent: ComponentRef<IconSecretComponent> = this.viewContainerRef.createComponent(
-      iconComponentFactory
-    );
+    const iconComponent: ComponentRef<IconSecretComponent> = this.viewContainerRef.createComponent(IconSecretComponent);
     iconComponent.instance.template = this.templateRef;
   }
 }
