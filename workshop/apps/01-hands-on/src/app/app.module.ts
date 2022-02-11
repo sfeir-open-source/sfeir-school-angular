69e4f80e0cf4b09815f36a8a8d9689566ecbe86c
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-02-11 21:28:08 +0100

    feat(workshop): :sparkles: migrate 01-hands-on workshop on nx architecture

---
+++ workshop/apps/01-hands-on/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MaterialDesignModule } from './material-design.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialDesignModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
