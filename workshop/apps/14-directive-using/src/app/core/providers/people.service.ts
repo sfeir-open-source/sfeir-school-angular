9244d694d35d966e5e615e38dee3f9c74b6ecffd
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-02-19 14:43:05 +0100

    feat(workshop): :sparkles: migrate workshop 15-directive-use on nx architecture

---
+++ workshop/apps/14-directive-using/src/app/core/providers/people.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { People } from '../../shared/models/people.model';
+import { CoreModule } from '../core.module';
+
+@Injectable({ providedIn: CoreModule })
+export class PeopleService {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getPeople(): Observable<Array<People>> {
+    return this.httpClient.get<Array<People>>(`${environment.peopleEndpoint}/peoples`);
+  }
+
+  getRandomPeople(): Observable<People> {
+    return this.httpClient.get<People>(`${environment.peopleEndpoint}/peoples/random`);
+  }
+
+  deletePeople(personId: string): Observable<Array<People>> {
+    return this.httpClient.delete<Array<People>>(`${environment.peopleEndpoint}/peoples/${personId}`);
+  }
+}
