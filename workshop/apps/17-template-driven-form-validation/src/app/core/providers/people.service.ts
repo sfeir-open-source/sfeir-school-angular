8865c3d4ce27da7dda76d16ae1c82ad3d6a568a9
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-02-20 08:09:25 +0100

    feat(workshop): :sparkles: migrate 19-form-validation workshop on nx architecture

---
+++ workshop/apps/17-template-driven-form-validation/src/app/core/providers/people.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { People, PeopleForm } from '../../shared/models/people.model';
+import { CoreModule } from '../core.module';
+
+@Injectable({ providedIn: CoreModule })
+export class PeopleService {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getPeople(): Observable<Array<People>> {
+    return this.httpClient.get<Array<People>>(`${environment.peopleEndpoint}/peoples`);
+  }
+
+  getRandomPeople(): Observable<People> {
+    return this.httpClient.get<People>(`${environment.peopleEndpoint}/peoples/random`);
+  }
+
+  deletePeople(personId: string): Observable<Array<People>> {
+    return this.httpClient.delete<Array<People>>(`${environment.peopleEndpoint}/peoples/${personId}`);
+  }
+
+  addNewPerson(person: PeopleForm): Observable<void> {
+    return this.httpClient.post<void>(`${environment.peopleEndpoint}/peoples`, person);
+  }
+}
---
+++ workshop/apps/17-template-driven-form-validation/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CardComponent } from './components/card/card.component';
+import { FormComponent } from './components/form/form.component';
+import { BadgeDirective } from './directives/badge.directive';
+import { MaterialDesignModule } from './material-design.module';
+import { NaPipe } from './pipes/na.pipe';
+
+@NgModule({
+  imports: [MaterialDesignModule, CommonModule, FormsModule],
+  exports: [MaterialDesignModule, CardComponent, BadgeDirective, FormComponent],
+  declarations: [CardComponent, NaPipe, BadgeDirective, FormComponent],
+})
+export class SharedModule {}
