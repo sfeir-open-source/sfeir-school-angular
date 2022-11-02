9f67bf88b9219277af780b4a0c99e40070e84c1b
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-11-02 16:34:32 +0100

    docs(doc): :arrow_up: upgrade sfeir school theme

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,11 +1,12 @@
-import { dayOneSlides } from './day_one.js';
-import { dayTwoSlides } from './day_two.js';
-import { dayThreeSLides } from './day_three.js';
+import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-school-theme.mjs';
 import { dayFourSlides } from './day_four.js';
+import { dayOneSlides } from './day_one.js';
+import { dayThreeSLides } from './day_three.js';
+import { dayTwoSlides } from './day_two.js';
 import { moduleDirectivSlides } from './directive-module.js';
 
 function sfeirInstitute() {
-  const formationSLides = [ ...dayOneSlides(), ...dayTwoSlides(), ...dayThreeSLides(), ...dayFourSlides() ];
+  const formationSLides = [...dayOneSlides(), ...dayTwoSlides(), ...dayThreeSLides(), ...dayFourSlides()];
   return formationSLides.map(slidePath => {
     return { path: slidePath };
   });
@@ -15,6 +16,4 @@ function sfeirModuleDirective() {
   return moduleDirectivSlides().map(slidePath => ({ path: slidePath }));
 }
 
-export function usedSlides() {
-  return sfeirInstitute();
-}
+SfeirThemeInitializer.init(sfeirInstitute);
