a38d4750634ee83ee3c5623a49582e55629cdbfd
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-11-21 11:42:18 +0100

    feat(workshop): :arrow_up: upgrade ngneat elf dependencies

--- apps/29-elf-store-solution/src/app/core/store/app.store.ts
+++ apps/29-elf-store-solution/src/app/core/store/app.store.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createStore, select, setProp, withProps } from '@ngneat/elf';
-import { selectAllApply, setEntities, withEntities } from '@ngneat/elf-entities';
+import { selectAllEntitiesApply, setEntities, withEntities } from '@ngneat/elf-entities';
 import { Observable, switchMap } from 'rxjs';
 import { People } from '../../shared/models/people.model';
 import { CoreModule } from '../core.module';
@@ -33,6 +33,6 @@ export class AppStoreService {
   }
 
   selectPeople(): Observable<Array<People>> {
-    return this.selectSearch().pipe(switchMap(search => this.APP_STORE.pipe(selectAllApply({ filterEntity: filteredPeople(search) }))));
+    return this.selectSearch().pipe(switchMap(search => this.APP_STORE.pipe(selectAllEntitiesApply({ filterEntity: filteredPeople(search) }))));
   }
 }
