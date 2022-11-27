a824819d23ce688993089fbc59e13e4625ae3b96
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-11-27 09:58:48 +0100

    feat(workshop): :arrow_up: migrate workshop 31 to last nx angular angular material version

--- apps/31-ngrx-store-solution/src/app/app-routing.module.ts
+++ apps/31-ngrx-store-solution/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: async () => (await import('./feature/home/home.module')).HomeModule },
-  { path: 'people', loadChildren: async () => (await import('./feature/people/people.module')).PeopleModule },
+  { path: 'home', loadChildren: async () => await import('./feature/home/home.module') },
+  { path: 'people', loadChildren: async () => await import('./feature/people/people.module') },
   {
     path: 'people/:id',
-    loadChildren: async () => (await import('./feature/update-person/update-person.module')).UpdatePersonModule,
+    loadChildren: async () => await import('./feature/update-person/update-person.module'),
   },
 ];
 
