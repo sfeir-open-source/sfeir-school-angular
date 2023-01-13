f03a57042bac02df22f7104ae0412b123c36e5a4
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2023-01-13 20:41:24 +0100

    fix(workshop):bug: incorrect usage of mat-card-image directive

--- apps/33-standalone-component-solution/src/app/core/interceptors/authorization.interceptor.ts
+++ apps/33-standalone-component-solution/src/app/core/interceptors/authorization.interceptor.ts
@@ -2,14 +2,6 @@ import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { People } from '../../shared/models/people.model';
 
-/** @Injectable()
-export class AuthorizationInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<People | Array<People>>, next: HttpHandler): Observable<HttpEvent<People | Array<People>>> {
-    const clonedRequest = request.clone({ setHeaders: { Authorization: 'Sfeir' } });
-    return next.handle(clonedRequest);
-  }
-} **/
-
 export function AuthorizationInterceptor(
   request: HttpRequest<People | Array<People>>,
   next: HttpHandlerFn
