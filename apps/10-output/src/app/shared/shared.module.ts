46a9093cedcacc7a475d56cbda62cecf1e63cf10
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-05-09 22:01:11 +0200

    test(workshop): ✅ add test for 10-output

--- apps/10-output/src/app/shared/shared.module.ts
+++ apps/10-output/src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { MaterialDesignModule } from './material-design.module';
 
 @NgModule({
-  imports: [MaterialDesignModule],
+  imports: [MaterialDesignModule, NgOptimizedImage],
   exports: [MaterialDesignModule, CardComponent],
   declarations: [CardComponent],
 })
