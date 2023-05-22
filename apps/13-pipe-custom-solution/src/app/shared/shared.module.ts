20be3e29509a31d712aa625ad9e28ea0dc26a472
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-05-22 21:56:15 +0200

    test(workshop): ✅ add missing test card for 13-custom-pipe workshop

--- apps/13-pipe-custom-solution/src/app/shared/shared.module.ts
+++ apps/13-pipe-custom-solution/src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { MaterialDesignModule } from './material-design.module';
 import { NaPipe } from './pipes/na.pipe';
 
 @NgModule({
-  imports: [MaterialDesignModule, CommonModule],
+  imports: [MaterialDesignModule, CommonModule, NgOptimizedImage],
   exports: [MaterialDesignModule, CardComponent],
   declarations: [CardComponent, NaPipe],
 })
