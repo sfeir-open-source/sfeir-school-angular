12cfb85522171d6aebc970933b895bffe4777eb9
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-05-30 20:51:13 +0200

    test(workshop): ✅ add missing test for 19-reactive-form

---
+++ apps/19-reactive-form-solution/src/app/feature/people/components/add-person-dialog/add-person-dialog.component.spec.ts
@@ -0,0 +1,50 @@
+import { AddPersonDialogComponent } from './add-person-dialog.component';
+import { fireEvent, render } from '@testing-library/angular';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { PeopleForm } from '../../../../shared/models/people.model';
+
+const DIALOG_REF = {
+  close: jest.fn(),
+};
+
+describe('AddPersonDialogComponent', () => {
+  let component: AddPersonDialogComponent;
+  let container: Element;
+
+  beforeEach(async () => {
+    const { fixture, container: hostContainer } = await render(AddPersonDialogComponent, {
+      providers: [{ provide: MatDialogRef, useValue: DIALOG_REF }],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+    component = fixture.componentInstance;
+    container = hostContainer;
+  });
+
+  it('should create an instance of AddPersonDialogComponent', () => {
+    expect(component).toBeInstanceOf(AddPersonDialogComponent);
+  });
+  it('should call the method closeDialog when cancel event is emit', () => {
+    const cancelEvent = new CustomEvent('cancel');
+    const spy = jest.spyOn(component, 'closeDialog');
+    const sfeirForm = container.querySelector('sfeir-form');
+    fireEvent(sfeirForm, cancelEvent);
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call the method closeDialog when save event is emit', () => {
+    const cancelEvent = new CustomEvent('save');
+    const spy = jest.spyOn(component, 'closeDialog');
+    const sfeirForm = container.querySelector('sfeir-form');
+    fireEvent(sfeirForm, cancelEvent);
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call the close method of the dialogRef', () => {
+    component.closeDialog();
+    expect(DIALOG_REF.close).toHaveBeenCalledWith(null);
+  });
+  it('should close the dialog with a person', () => {
+    const person = { firstname: 'toto' } as PeopleForm;
+    component.closeDialog(person);
+    expect(DIALOG_REF.close).toHaveBeenCalledWith(person);
+  });
+});
