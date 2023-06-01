5bdac8470ce418bb13428fc3b48cd29a703dedc8
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-06-01 11:23:16 +0200

    test(workshop): ✅ add missing test for 27 structural-directive

--- apps/26-hostbinding-hostlistener/src/app/app.module.ts
+++ apps/26-hostbinding-hostlistener/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
+import { CoreModule } from '../../../25-teleportation-solution/src/app/core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, HttpClientModule, AppRoutingModule, MatToolbarModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, HttpClientModule, AppRoutingModule, MatToolbarModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
