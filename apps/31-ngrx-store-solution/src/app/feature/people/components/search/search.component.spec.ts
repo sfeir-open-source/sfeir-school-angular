b43d2c4b3739e414cc5836e51f90bf81a541e59a
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-06-10 17:11:21 +0200

    test(workshop): ✅ add missing test for 31 workshop

---
+++ apps/31-ngrx-store-solution/src/app/feature/people/components/search/search.component.spec.ts
@@ -0,0 +1,39 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { fireEvent, render } from '@testing-library/angular';
+import { SearchComponent } from './search.component';
+
+const SEARCH_SPY = jest.fn();
+
+describe('SearchComponent', () => {
+  let container: Element;
+  let component: SearchComponent;
+
+  beforeEach(async () => {
+    const { fixture, container: hostContainer } = await render(SearchComponent, {
+      imports: [ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      componentProperties: {
+        searchText: 'SFEIR',
+        search: {
+          emit: SEARCH_SPY,
+        } as any,
+      },
+    });
+    container = hostContainer;
+    component = fixture.componentInstance;
+  });
+
+  test('should create an instance of search', () => {
+    expect(component).toBeInstanceOf(SearchComponent);
+  });
+  test('should bind correctly the input', async () => {
+    const element = container.querySelector<HTMLInputElement>('input[placeholder="Person Searcher"]');
+    expect(element.value).toBe('SFEIR');
+  });
+  test('should emit the search event', () => {
+    const element = container.querySelector<HTMLInputElement>('input[placeholder="Person Searcher"]');
+    fireEvent.input(element, { target: { value: 'test' } });
+    expect(SEARCH_SPY).toHaveBeenCalledWith('test');
+  });
+});
