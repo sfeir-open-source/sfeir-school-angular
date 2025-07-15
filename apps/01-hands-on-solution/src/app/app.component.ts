9618414aec586581f58a0822f6f3b275132ba34d
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2025-07-15 16:26:18 +0200

    refactor(app:01-hands-on): bootstrap now on the component and not on the module

--- apps/01-hands-on-solution/src/app/app.component.ts
+++ apps/01-hands-on-solution/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'sfeir-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: false,
+  imports: [MatCardModule, MatToolbarModule],
 })
 export class AppComponent {
   name = 'SFEIR - LUXEMBOURG';
--- apps/01-hands-on/src/main.ts
+++ apps/01-hands-on/src/main.ts
@@ -1,13 +1,10 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication().then(console.log).catch(console.error);
+enableProdMode();
