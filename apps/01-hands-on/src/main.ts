1d56be8693fbed76f807e1337243b25635eb176b
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2025-08-07 15:42:34 +0200

    fix(apps): regression build and test after rework

--- apps/01-hands-on/src/main.ts
+++ apps/01-hands-on/src/main.ts
@@ -1,10 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication().then(console.log).catch(console.error);
 enableProdMode();
