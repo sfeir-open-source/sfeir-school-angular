02b327815b17d23acbfdb07dc57ca1a77e420f29
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2026-07-04 00:26:33 +0200

    feat(apps): add signal form workshop

---
+++ apps/19-signal-form-solution/src/app/shared/components/form/people-form.ts
@@ -0,0 +1,27 @@
+import { WritableSignal } from '@angular/core';
+import { apply, email, form, minLength, pattern, required, schema } from '@angular/forms/signals';
+
+export type Person = {
+  id: string;
+  photo: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+};
+
+export const namesSchema = schema<string>(names => {
+  required(names, { message: 'This field is required' });
+  minLength(names, 2, { message: 'This field must be at least 2 characters long' });
+});
+
+const schemaPerson = schema<Person>(person => {
+  required(person.photo);
+  apply(person.firstname, namesSchema);
+  apply(person.lastname, namesSchema);
+  email(person.email, { message: 'This field must be a valid email address' });
+  pattern(person.phone, /\d{10}/, { message: 'This field must be a valid phone number' });
+});
+
+export const createPeopleForm = (person: WritableSignal<Person>, callBackSubmit: () => void) =>
+  form(person, schemaPerson, { submission: { action: async () => callBackSubmit() } });
